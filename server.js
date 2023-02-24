283608326f2fdfe071b7c47b4780decfa0285ca4
Anshita Goyal <anshitagoyal181@gmail.com>
2023-02-24 12:33:06 +0000

    Updated server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
       </head>
       <body>
         <section>
-          <h2>My Course Goals are: check 0009 </h2>
+          <h2>My Course Goals are: check 10</h2>
           <h3>${userGoal}</h3>
         </section>
         <form action="/store-goal" method="POST">
